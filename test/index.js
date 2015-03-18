7da6e82fec48e67983b2f5896c2cd2a70b6e4490
Nicolas Perriault <nperriault@gmail.com>
2015-03-18 18:32:17 +0100

    Switched to using jsdom for tests.

--- test/index.js
+++ test/index.js
@@ -1,11 +1,8 @@
 var path = require("path");
 var fs = require("fs");
+var jsdom = require("jsdom");
 var prettyPrint = require("html").prettyPrint;
-var chai = require("chai");
-var chaiAsPromised = require("chai-as-promised");
-chai.should();
-chai.use(chaiAsPromised);
-var expect = chai.expect;
+var expect = require("chai").expect;
 
 var testPageRoot = path.join(__dirname, "test-pages");
 var testPages = fs.readdirSync(testPageRoot).map(function(dir) {
@@ -17,32 +14,30 @@ var testPages = fs.readdirSync(testPageRoot).map(function(dir) {
 });
 
 describe("Test page", function() {
-  var oldLibPath = process.env.READABILITY_LIB_PATH;
-  process.env.READABILITY_LIB_PATH = path.join(__dirname, "..", "Readability.js");
-  var scrape = require("readable-proxy").scrape;
   testPages.forEach(function(testPage) {
     describe(testPage.dir, function() {
-      it("should render as expected", function() {
-        // Allows up to 10 seconds for parsing to complete.
-        // XXX: Scraping is damn slow. Investigate.
-        this.timeout(10000);
+      it("should render as expected", function(done) {
         var expected = fs.readFileSync(testPage.expected, {encoding: "utf-8"});
-        return scrape("file://" + testPage.source).catch(function(err) {
-          throw err;
-        }).then(function(result) {
-          // print Readability log messages
-          (result.consoleLogs || [])
-            .filter(function(logMessage) {
-              return logMessage.indexOf("Reader: (Readability)") === 0;
-            })
-            .forEach(function(logMessage) {
-              console.log("[LOG]", logMessage);
-            });
-          // normalize html
-          return prettyPrint(result.content);
-        }).should.eventually.become(prettyPrint(expected));
+        jsdom.env(
+          fs.readFileSync(testPage.source, {encoding: "utf-8"}),
+          [path.join(__dirname, "..", "Readability.js")],
+          function (errors, window) {
+            if (errors) {
+              throw new Error(errors);
+            }
+            var uri = {
+              spec: "http://fakehost/test/page.html",
+              host: "fakehost",
+              prePath: "http://fakehost",
+              scheme: "http",
+              pathBase: "http://fakehost/test"
+            };
+            var result = new window.Readability(uri, window.document).parse();
+            expect(prettyPrint(result.content)).eql(prettyPrint(expected))
+            done();
+          }
+        );
       });
     });
   });
-  process.env.READABILITY_LIB_PATH = oldLibPath;
 });
