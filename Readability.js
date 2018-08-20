f782bc5f06a139827c87969a144ef14d5159cfd8
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2018-08-20 17:40:00 +0100

    Avoid global flag when looking for metadata using regexes

--- Readability.js
+++ Readability.js
@@ -1200,10 +1200,10 @@ Readability.prototype = {
 
     // Match "description", or Twitter's "twitter:description" (Cards)
     // in name attribute.
-    var namePattern = /^\s*((twitter)\s*:\s*)?(description|title)\s*$/gi;
+    var namePattern = /^\s*((twitter)\s*:\s*)?(description|title)\s*$/i;
 
     // Match Facebook's Open Graph title & description properties.
-    var propertyPattern = /^\s*og\s*:\s*(description|title)\s*$/gi;
+    var propertyPattern = /^\s*og\s*:\s*(description|title)\s*$/i;
 
     // Find description tags.
     this._forEachNode(metaElements, function(element) {
