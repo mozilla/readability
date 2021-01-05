e1c7905288d5094be88400ba50b7b5969fc99adc
Mathieu Dutour <mathieu@dutour.me>
2021-01-05 15:11:05 +0100

    Add typescript definition (#667)
    Add a typescript definition with a generic serializer.

---
+++ index.d.ts
@@ -0,0 +1,49 @@
+/**
+ * Decides whether or not the document is reader-able without parsing the whole thing.
+ * @return {boolean} Whether or not we suspect Readability.parse() will suceeed at returning an article object.
+ */
+export function isProbablyReaderable(
+  document: Document,
+  options?: {
+    /** The minimum node content length used to decide if the document is readerable. */
+    minContentLength?: number;
+    /** The minumum cumulated 'score' used to determine if the document is readerable. */
+    minScore?: number;
+    /** The function used to determine if a node is visible. */
+    visibilityChecker?: (node: Node) => boolean;
+  }
+): boolean;
+
+export class Readability<T = string> {
+  constructor(
+    document: Document,
+    options?: {
+      debug?: boolean;
+      maxElemsToParse?: number;
+      nbTopCandidates?: number;
+      charThreshold?: number;
+      classesToPreserve?: string[];
+      keepClasses?: boolean;
+      serializer?: (node: Node) => T;
+      disableJSONLD?: boolean;
+    }
+  );
+
+  parse(): null | {
+    /** article title */
+    title: string;
+    /** author metadata */
+    byline: string;
+    /** content direction */
+    dir: string;
+    /** HTML of processed article content */
+    content: T;
+    /** text content of the article (all HTML removed) */
+    textContent: string;
+    /** length of an article, in characters */
+    length: number;
+    /** article description, or short excerpt from the content */
+    excerpt: string;
+    siteName: string;
+  };
+}
