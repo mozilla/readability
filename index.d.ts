91868f04630c132da324d5a12662c557d46b6cb7
Teddy Bradford <3684553+teddybradford@users.noreply.github.com>
2022-02-18 19:02:59 -0500

    Fix `parse()` types indentation

--- index.d.ts
+++ index.d.ts
@@ -29,32 +29,32 @@ export class Readability<T = string> {
     }
   );
 
-parse(): null | {
-  /** article title */
-  title: string;
+  parse(): null | {
+    /** article title */
+    title: string;
 
-  /** HTML string of processed article content */
-  content: T;
+    /** HTML string of processed article content */
+    content: T;
 
-  /** text content of the article, with all the HTML tags removed */
-  textContent: string;
+    /** text content of the article, with all the HTML tags removed */
+    textContent: string;
 
-  /** length of an article, in characters */
-  length: number;
+    /** length of an article, in characters */
+    length: number;
 
-  /** article description, or short excerpt from the content */
-  excerpt: string;
+    /** article description, or short excerpt from the content */
+    excerpt: string;
 
-  /** author metadata */
-  byline: string;
+    /** author metadata */
+    byline: string;
 
-  /** content direction */
-  dir: string;
+    /** content direction */
+    dir: string;
 
-  /** name of the site */
-  siteName: string;
+    /** name of the site */
+    siteName: string;
 
-  /** content language */
-  lang: string;
-};
+    /** content language */
+    lang: string;
+  };
 }
