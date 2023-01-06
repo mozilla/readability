76bfa3f37c675436d0e373299de5b47cbb6a706c
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2023-01-06 12:45:17 -0500

    Separate regexes into new file

---
+++ Readability-candidate-regexes.js
@@ -0,0 +1,28 @@
+var REGEXPS = {
+  // The following two are used by both Readability.js and Readability-readerable.js
+  unlikelyCandidates: /-ad-|ai2html|banner|breadcrumbs|combx|comment|community|cover-wrap|disqus|extra|footer|gdpr|header|legends|menu|related|remark|replies|rss|shoutbox|sidebar|skyscraper|social|sponsor|supplemental|ad-break|agegate|pagination|pager|popup|yom-remote/i,
+  okMaybeItsACandidate: /and|article|body|column|content|main|shadow/i,
+
+  positive: /article|body|content|entry|hentry|h-entry|main|page|pagination|post|text|blog|story/i,
+  negative: /-ad-|hidden|^hid$| hid$| hid |^hid |banner|combx|comment|com-|contact|foot|footer|footnote|gdpr|masthead|media|meta|outbrain|promo|related|scroll|share|shoutbox|sidebar|skyscraper|sponsor|shopping|tags|tool|widget/i,
+  extraneous: /print|archive|comment|discuss|e[\-]?mail|share|reply|all|login|sign|single|utility/i,
+  byline: /byline|author|dateline|writtenby|p-author/i,
+  replaceFonts: /<(\/?)font[^>]*>/gi,
+  normalize: /\s{2,}/g,
+  videos: /\/\/(www\.)?((dailymotion|youtube|youtube-nocookie|player\.vimeo|v\.qq)\.com|(archive|upload\.wikimedia)\.org|player\.twitch\.tv)/i,
+  shareElements: /(\b|_)(share|sharedaddy)(\b|_)/i,
+  nextLink: /(next|weiter|continue|>([^\|]|$)|»([^\|]|$))/i,
+  prevLink: /(prev|earl|old|new|<|«)/i,
+  tokenize: /\W+/g,
+  whitespace: /^\s*$/,
+  hasContent: /\S$/,
+  hashUrl: /^#.+/,
+  srcsetUrl: /(\S+)(\s+[\d.]+[xw])?(\s*(?:,|$))/g,
+  b64DataUrl: /^data:\s*([^\s;,]+)\s*;\s*base64\s*,/i,
+  // See: https://schema.org/Article
+  jsonLdArticleTypes: /^Article|AdvertiserContentArticle|NewsArticle|AnalysisNewsArticle|AskPublicNewsArticle|BackgroundNewsArticle|OpinionNewsArticle|ReportageNewsArticle|ReviewNewsArticle|Report|SatiricalArticle|ScholarlyArticle|MedicalScholarlyArticle|SocialMediaPosting|BlogPosting|LiveBlogPosting|DiscussionForumPosting|TechArticle|APIReference$/
+}
+
+if (typeof module === "object") {
+  module.exports = REGEXPS;
+}
