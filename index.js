7150a922e983a7bfbb8cf659aaff130d19537893
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2023-01-06 12:49:21 -0500

    Export regexps

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var Readability = require("./Readability");
 var isProbablyReaderable = require("./Readability-readerable");
+var REGEXPS = require("./Readability-regexps");
 
 module.exports = {
   Readability: Readability,
-  isProbablyReaderable: isProbablyReaderable
+  isProbablyReaderable: isProbablyReaderable,
+  REGEXPS: REGEXPS
 };
