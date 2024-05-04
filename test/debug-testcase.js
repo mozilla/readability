cc4f5341a0f2bc4e163ed09c1505403ba47fc13e
Shaun Martin <inhumantsar@protonmail.com>
2024-05-04 16:54:02 -0500

    update changelog, fix linting errors

--- test/debug-testcase.js
+++ test/debug-testcase.js
@@ -1,4 +1,6 @@
-var Readability = require('../Readability');
+/* eslint-env node */
+
+var Readability = require("../Readability");
 var {JSDOM} = require("jsdom");
 var fs = require("fs");
 var path = require("path");
@@ -6,8 +8,8 @@ var path = require("path");
 var testcaseRoot = path.join(__dirname, "test-pages");
 
 if (process.argv.length < 3) {
-console.log("No testcase provided.");
-process.exit(1);
+  console.log("No testcase provided.");
+  process.exit(1);
 }
 
 var src = fs.readFileSync(`${testcaseRoot}/${process.argv[2]}/source.html`, {encoding: "utf-8"}).trim();
